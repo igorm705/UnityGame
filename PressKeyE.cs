21cfd6089659a4c6fcb39bf903fb32d6a8445712
igorm705 <33665077+igorm705@users.noreply.github.com>
2019-03-19 18:08:29 +0200

    Add files via upload

---
+++ PressKeyE.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressKeyE : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var bc = this.GetComponent<PressKeyE>();
+        bc.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+        }
+    }
+}
