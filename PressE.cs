63456a44e9c0a17058e80730231e773e0991ee81
igorm705 <33665077+igorm705@users.noreply.github.com>
2019-04-28 19:14:06 +0300

    Update PressE.cs

--- PressE.cs
+++ PressE.cs
@@ -1,38 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Important!!! In order to Ellen will able to interact with GameObject that
-//holds this script, turn Layer of the GameObject to "Environment"
-
-public class PressE : MonoBehaviour
+public class PressKeyE : MonoBehaviour
 {
+    const int ARRAY_SIZE = 2;
 
-    public GameObject uiObject;
+    public GameObject[] camera1 = new GameObject[ARRAY_SIZE];
 
+    public GameObject NumbersController;
     // Start is called before the first frame update
     void Start()
     {
-        uiObject.SetActive(false);
+        var bc = this.GetComponent<PressKeyE>();
+        bc.enabled = false;
     }
 
-    // trigger is called 
-    private void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    void Update()
     {
-
-        if (other.gameObject.name == "Ellen")
+      
+        if (Input.GetKey("e"))
         {
-            uiObject.SetActive(true);
-            StartCoroutine("WaitForSec");
-        }
+            //change cameras
+            camera1[0].SetActive(true);
+            camera1[1].SetActive(false);
+
+            //activate "Update function" of "NumbersControllersScript"
+            var start = NumbersController.GetComponent<NumbersControllersScript>();
+            start.start_sort = true;
+             
+        }      
     }
-
-        IEnumerator WaitForSec()
-        {
-            yield return new WaitForSeconds(5);
-            uiObject.SetActive(false);
-        }
-
-   
-    
 }
