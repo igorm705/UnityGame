745e1fd44f29928ceba89944ca41383de59b133f
igorm705 <33665077+igorm705@users.noreply.github.com>
2019-04-28 19:18:54 +0300

    Update PressKeyE.cs

--- PressKeyE.cs
+++ PressKeyE.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PressKeyE : MonoBehaviour
 {
+    const int ARRAY_SIZE = 2;
 
+    public GameObject[] camera1 = new GameObject[ARRAY_SIZE];
 
+    public GameObject NumbersController;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,17 @@ public class PressKeyE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+      
+        if (Input.GetKey("e"))
         {
-            print("space key was pressed");
-        }
+            //change cameras
+            camera1[0].SetActive(true);
+            camera1[1].SetActive(false);
+
+            //activate "Update function" of "NumbersControllersScript"
+            var start = NumbersController.GetComponent<NumbersControllersScript>();
+            start.start_sort = true;
+             
+        }      
     }
 }
